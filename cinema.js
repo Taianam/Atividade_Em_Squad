5171d592b5fc8534a90477e4a1938f9cc55a1b16
Taianam <taiana.monteirof@gmail.com>
2021-04-09 10:55:21 -0300

    ok

--- cinema.js
+++ cinema.js
@@ -1,65 +1,57 @@
-const ler = require("prompt-sync")();
+/*1. Um cinema faz reservas de 5 ingressos para uma sala, sendo 2 para meia 
+entrada, para a reserva, basta informar o nome e se é meia ou não, é 
+permitido apenas uma reserva por nome.
+2. Após os 5 reservados, o cliente pode retirar se ingresso informando o seu 
+nome, o ingresso irá informar seu número, nome de quem reservou e se é meia
+ ou não.
+ tipo ingresso ok
+ nome pessoa ok
+ 
+ */
+ 
 
-var nome1 = "";
-var nome2 = "";
-var nome3 = "";
-var nome4 = "";
-var nome5 = "";
-var contador = 0;
-var assentoComDesconto1 = "";
-var assentoComDesconto2 = "";
+ const prompt = require ("prompt-sync")();
+ //3 INTEIRAS E 2 MEIAS
+ var inteira = 0;
+ var meia = 0;
+ // const LIMITE =5;
+ var cliente = 0;
+ var nomes = [];
+ var tipoIngresso = [];
+ 
+ console.log("== CINEMA TCHOOOOOOP ==")
+ 
+ while(nomes.length < 5){
+   var nome = prompt("Digite seu nome por favor")
+   var nomeRepetido = false;
+   for (j = 0; j <= nomes.length; j++){
+       if(nomes[j] == nome){
+           nomeRepetido == true;
+           break
+       }
+      if(nomeRepetido){
+          console.log("Essse nome já consta em nossa base");
+          continue;
+      }
+   }
+ 
+     nomes.push(nome);
+ 
+   var pagante = prompt("Senhor " + nome + " você deseja um ingresso inteiro ou meia? obs. existem 2 ingressos meia e 3 inteiros")
+ 
+   if(pagante == "meia"){
+ 
+       if(meia == 2){
+         console.log("Senhor cliente os ingressos meios acabaram seu ingresso será inteiro")
+         pagante = "inteira"
+     }else {
+     meia++
+     }
+     }
+     tipoIngresso.push(pagante);
+ 
+ }
 
-for (let index = 0; index < 5; index++) {
-    
-    var nome = ler("Digite o nome  para colocar no ingresso?");
-    var condicao = false;
-
-    if(validarNome()){
-        condicao = true;
-    }
-
-    while (condicao) {
-        nome = ler("Digite o nome  para colocar no ingresso?");
-        if(validarNome()){
-            condicao = true;
-        }else{
-            condicao = false;
-        }
-    }
-
-    if(contador < 2){
-        var desconto = ler("Digite o nome  para colocar no ingresso?");
-            if( desconto == "sim"){
-                contador++;
-                if(assentoComDesconto1 == ""){
-                    assentoComDesconto1 = nome;
-                }else{
-                    assentoComDesconto2 = nome;
-                }
-            }
-    }
-
-    if( nome1 == ""){
-        nome1 = nome;
-    }else if(nome2 == ""){
-        nome2 = nome;
-    }else if(nome3 == ""){
-        nome3 = nome;
-    }else if(nome4 == ""){
-        nome4 = nome;
-    }else if(nome5 == ""){
-        nome5 = nome;
-    }
-}
-
-function validarNome(){
-    if(nome1 === nome || nome2 === nome || nome3 === nome || nome4 === nome || nome5 === nome){
-        console.log("Esse nome já existe");
-        return true;
-    }else{
-        return false;
-    }
-}
-
-console.log("Igresso vendido:", nome1, nome2, nome3,nome4,nome5);
-console.log("Ingresso meia entrada:", assentoComDesconto1, assentoComDesconto2);
+ for(i = 0; i < 5; i++){
+ console.log("\nNome: " + nomes[i] + "\nNumero ingresso: "+ i + "\nTipo ingresso: " + tipoIngresso[i]);
+ }
